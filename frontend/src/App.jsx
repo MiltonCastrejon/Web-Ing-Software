fb457a222344b42d439c71c55ecc30575a9f5d8f
Milton Castrejon <aldair0624@gmail.com>
2023-06-14 23:01:25 -0500

    Segundo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,35 +6,27 @@ import { Sidebar } from './components/Sidebar';
 
 function App() {
   const [sidebarOpen, setsidebarOpen] = useState(false);
-  const getCurrentPage = () => {
-    const { pathname } = location;
-    if (pathname === '/login') {
-      return 'login';
-    }    
-  }
-  const currentPage = getCurrentPage();
 
   return (
     <>
       <BrowserRouter>
-      {currentPage !== 'login' && ( 
-          <Container className={sidebarOpen ? 'sidebarState Activado' : 'sidebarState'}>
-            <Sidebar sidebarOpen={sidebarOpen} setsidebarOpen={setsidebarOpen} />
-            <Rutas />
-          </Container>
-        )}
-        {currentPage === 'login' && <Login />} // Renderiza la página de inicio de sesión si es la página actual
+        <Container
+          className={sidebarOpen ? 'sidebarState Activado' : 'sidebarState'}
+        >
+          <Sidebar sidebarOpen={sidebarOpen} setsidebarOpen={setsidebarOpen} />
+          <Rutas />
+        </Container>
       </BrowserRouter>
     </>
   );
 }
 
 const Container = styled.div`
-  display: grid ;
+  display: grid;
   height: 100vh;
   grid-template-columns: 90px auto;
   background: #f5f5f5;
-  &.Activado{
+  &.Activado {
     grid-template-columns: 250px auto;
   }
 `;
